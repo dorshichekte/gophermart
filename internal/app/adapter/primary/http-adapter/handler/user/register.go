c9408736bb9a3ec682e866da77079992bf0983a0
dorshichekte <vdpntr@yandex.ru>
2025-06-17 17:36:26 +0300

    add test logs

--- internal/app/adapter/primary/http-adapter/handler/user/register.go
+++ internal/app/adapter/primary/http-adapter/handler/user/register.go
@@ -24,6 +24,7 @@ func (uh *Handler) Register(auth auth.Auth) func(res http.ResponseWriter, req *h
 		var registerRequestDto dto.RegisterRequest
 		decodeErr := uh.DecodeJSON(req, &registerRequestDto)
 		if decodeErr != nil {
+			uh.Logger.Error(decodeErr.Error())
 			util.WriteErrorResponse(res, http.StatusBadRequest, util.WrapperError[string]{CustomError: decodeErr.Error()})
 			return
 		}
@@ -32,16 +33,19 @@ func (uh *Handler) Register(auth auth.Auth) func(res http.ResponseWriter, req *h
 		if validateErr != nil {
 			validationErrors, parseError := uh.validator.ParseValidationErrors(validateErr)
 			if parseError != nil {
+				uh.Logger.Error(parseError.Error())
 				util.WriteErrorResponse(res, http.StatusBadRequest, util.WrapperError[string]{CustomError: validateErr.Error()})
 				return
 			}
 
+			uh.Logger.Error(validateErr.Error())
 			util.WriteErrorResponse(res, http.StatusBadRequest, util.WrapperError[[]v.ValidationError]{CustomError: validationErrors})
 			return
 		}
 
 		userID, registerErr := uh.Service.User.Register(ctx, registerRequestDto.Login, registerRequestDto.Password)
 		if registerErr != nil {
+			uh.Logger.Error(registerErr.Error())
 			if errors.Is(registerErr, user_usecase.ErrLoginAlreadyTaken) {
 				util.WriteErrorResponse(res, http.StatusConflict, util.WrapperError[string]{CustomError: registerErr.Error()})
 				return
@@ -53,6 +57,7 @@ func (uh *Handler) Register(auth auth.Auth) func(res http.ResponseWriter, req *h
 
 		authData, setAuthErr := auth.Generate(userID)
 		if setAuthErr != nil {
+			uh.Logger.Error(setAuthErr.Error())
 			util.WriteErrorResponse(res, http.StatusInternalServerError, util.WrapperError[string]{CustomError: setAuthErr.Error()})
 			return
 		}
