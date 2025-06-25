321d70f161d2df41d434097bf9a908b8b5ab8869
dorshichekte <vdpntr@yandex.ru>
2025-06-25 17:52:30 +0300

    fix length login + password

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id  BIGSERIAL PRIMARY KEY,
-    login VARCHAR(255) UNIQUE NOT NULL CHECK (length(login) >= 8),
-    password VARCHAR(255) NOT NULL CHECK (length(password) >= 8),
+    login VARCHAR(255) UNIQUE NOT NULL CHECK (length(login) >= 4),
+    password VARCHAR(255) NOT NULL CHECK (length(password) >= 6),
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
